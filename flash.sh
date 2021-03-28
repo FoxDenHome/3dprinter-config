244327b224e733b4219e76219928d1a09f0cd01e
Mark Dietzer <git@doridian.net>
2021-03-28 10:31:09 -0700

    sudo

--- flash.sh
+++ flash.sh
@@ -2,6 +2,6 @@
 cd ~/klipper
 
 make
-/usr/sbin/service klipper stop
+sudo /usr/sbin/service klipper stop
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f103xe_30FFD5054254353919561557-if00 btt-skr-mini
-/usr/sbin/service klipper start
+sudo /usr/sbin/service klipper start
