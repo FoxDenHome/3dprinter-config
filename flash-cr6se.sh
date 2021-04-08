7c9585983cd6c402cbb397170abdf4afd072a792
Mark Dietzer <git@doridian.net>
2021-04-07 19:41:16 -0700

    clean

--- flash-cr6se.sh
+++ flash-cr6se.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ~/klipper
 
+make clean
 make
 sudo /usr/sbin/service klipper stop
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f103xe_30FFD5054254353919561557-if00 btt-skr-mini
