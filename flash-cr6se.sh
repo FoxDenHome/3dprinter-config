41d9d17bd273d3dd3abffe871ac98c26bcee8278
Mark Dietzer <git@doridian.net>
2021-04-07 19:50:00 -0700

    Full auto

--- flash-cr6se.sh
+++ flash-cr6se.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 cd ~/klipper
 
+git pull -r
 make clean
 make
+
 sudo /usr/sbin/service klipper stop
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f103xe_30FFD5054254353919561557-if00 btt-skr-mini
 sudo /usr/sbin/service klipper start
+
