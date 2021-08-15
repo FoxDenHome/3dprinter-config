aa5f8890c6c3a27014d004e3df363947605f0d80
Mark Dietzer <git@doridian.net>
2021-08-14 21:05:13 -0700

    swap to homeassistant

--- sanitize.sh
+++ sanitize.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed 's~password:.*~password: REMOVED~g' moonraker.conf > moonraker-stripped.conf
+sed 's~password:.*~password: REMOVED~g' moonraker.conf | sed 's~token:.*~token: REMOVED~g' > moonraker-stripped.conf
