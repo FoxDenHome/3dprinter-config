d01a1c7a6957100ffbf53670d6c439a23162a94e
Mark Dietzer <git@doridian.net>
2022-01-18 08:37:37 -0800

    python3

--- flash-cr6se.sh
+++ flash-cr6se.sh
@@ -2,8 +2,8 @@
 cd ~/klipper
 
 git pull -r
-make clean
-make
+make clean PYTHON=python3
+make PYTHON=python3
 
 sudo /usr/sbin/service klipper stop
 ./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_stm32f103xe_30FFD5054254353919561557-if00 btt-skr-mini
